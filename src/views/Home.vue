922724174a0294c42390ebc6e29bb7be0380fbd5
Lil <lil@Lils-MacBook-Air.local>
2021-03-29 18:48:52 -0500

    Movie app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,111 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <!-- Index -->
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>{{ movie.title }}</h2>
+      <h3>Director: {{ movie.director }}, Year: {{ movie.year }}</h3>
+      <p> {{ movie.plot }} </p>
+    <!-- Show -->
+      <button v-on:click="movieShow(movie)">More Info</button>
+      <dialog id="movie-info">
+        <form method="dialog">
+          Title: <input type="text" v-model="currentMovie.title"> <br>
+          Year: <input type="text" v-model="currentMovie.year"> <br>
+          Director: <input type="text" v-model="currentMovie.director"> <br>
+          Plot: <input type="text" v-model="currentMovie.plot"> <br> <br>
+          <button>Close</button>
+          <button v-on:click="movieUpdate(currentMovie)">Update</button>
+          <button v-on:click="movieDelete()">Delete</button>
+        </form>
+      </dialog>
+    </div>
+    <br>
+    <!-- Create -->
+    <div id="create">
+      <h3>Add New Movie</h3>
+      Title: <input type="text" v-model="movieTitle"> <br>
+      Year: <input type="text" v-model="movieYear"> <br>
+      Director: <input type="text" v-model="movieDirector"> <br>
+      Plot: <input type="text" v-model="moviePlot"> <br> <br>
+    <button v-on:click="moviesCreate()">Add Movie</button>
+    </div>
   </div>
 </template>
+<style>
+div {
+  color: white;
+  background-color: rgb(245, 166, 180);
+}
+#create {
+  background-color: silver;
+  width: 250px;
+  margin: 0 auto;
+  padding: 15px;
+}
+</style>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Lil's Fav Movies",
+      movies: [],
+      movieTitle: "",
+      movieYear: "",
+      movieDirector: "",
+      moviePlot: "",
+      currentMovie: {},
+    };
+  },
+  created: function () {
+    this.moviesIndex();
+  },
+  methods: {
+    moviesIndex: function () {
+      axios.get("/api/movies").then((response) => {
+        this.movies = response.data;
+        console.log(response.data);
+      });
+    },
+    moviesCreate: function () {
+      let params = {
+        title: this.movieTitle,
+        year: this.movieYear,
+        director: this.movieDirector,
+        plot: this.moviePlot,
+      };
+      axios.post("/api/movies/", params).then(
+        (response) => {
+          console.log(response);
+          this.movies.push(response.data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    movieShow: function (movie) {
+      this.currentMovie = movie;
+      document.querySelector("#movie-info").showModal();
+    },
+    movieUpdate: function (movie) {
+      let params = {
+        title: this.movvieTitle,
+        year: this.movieYear,
+        director: this.movieDirector,
+        plot: this.moviePlot,
+      };
+      axios.patch("/api/movies/" + movie.id, params).then((response) => {
+        console.log("Movie updated." + response);
+      });
+    },
+    movieDelete: function (movie) {
+      axios.delete("/api/movies/" + movie.id);
+      console.log("Successfully deleted.");
+    },
   },
 };
 </script>
